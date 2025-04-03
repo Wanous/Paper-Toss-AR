b7b9177afad921ef09c499de3b36b2e941ff743b
Wanous <163208990+Wanous@users.noreply.github.com>
2025-04-03 16:19:55 +0200

    first commit

---
+++ Paper Toss AR/Assets/Scripts/KillingCam.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.InputSystem;
+
+
+public class KillingCam : MonoBehaviour
+{
+
+    public GameObject ParticleEffect;
+    private Vector2 touchPos;
+    private RaycastHit hit;
+    private Camera cam;
+
+    public PlayerInput playerInput;
+    private InputAction touchPressAction;
+    private InputAction touchPosAction;
+
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        touchPressAction = playerInput.actions["TouchPress"];
+        touchPosAction = playerInput.actions["TouchPos"];
+    }
+
+    void Update()
+    {
+        if (!touchPressAction.WasPerformedThisFrame())
+        {
+            return;
+        }
+
+        touchPos = touchPosAction.ReadValue<Vector2>();
+        Ray ray = cam.ScreenPointToRay(touchPos);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            GameObject hitObj = hit.collider.gameObject;
+            if (hitObj.tag == "Enemy")
+            {
+                var clone = Instantiate(ParticleEffect, hitObj.transform.position, Quaternion.identity);
+                clone.transform.localScale = hitObj.transform.localScale;
+                Destroy(hitObj);
+            }
+        }
+    }
+}
